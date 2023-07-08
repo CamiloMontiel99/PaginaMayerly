3c509b1265bcae656f5b322f39d1a8ec1008b605
CamiloMontiel99 <camilo__developer@hotmail.com>
2023-07-08 15:48:08 -0500

    portafolioMayerly

--- JS/main.js
+++ JS/main.js
@@ -13,54 +13,7 @@ $(document).ready(function(){
     })
 })
 
-
-// sliderClientes:
-
-// const sliderClientes = document.querySelector(".slide-items");
-
-// let maxScrollLeft = sliderClientes.scrollWidth - sliderClientes.clientWidth;
-// let intervalo = null;
-// let step = 2;
-
-// const start = () => {
-//     intervalo = setInterval(function() {
-//         sliderClientes.scrollLeft = sliderClientes.scrollLeft + step;
-//         if (sliderClientes.scrollLeft === maxScrollLeft) {
-//             step = step * -1;
-//         } else if (sliderClientes.scrollLeft === 0) {
-//             step = step * -1;
-//         }
-//     }, 10);
-// }
-
-// const stop = () => {
-//     clearInterval(intervalo);
-// };
-
-// sliderClientes.addEventListener('mouseover', () => {
-//     stop();
-// })
-
-// sliderClientes.addEventListener('mouseout', () => {
-//     start();
-// })
-
-// start();
-
-
-// Contact Us:
-
-// let inputs = document.getElementsByClassName('formulario__input');
-// for (let i = 0; i < inputs.length; i++) {
-//     inputs[i].addEventListener('keyup', function(){
-//     if(this.value.length>=1) {
-//         this.nextElementSibling.classList.add('formulario__label');
-//     } else {
-//         this.nextElementSibling.classList.remove('formulario__label');
-//         }
-//     });
-// }
-
+// Input Number, eliminar negativo:
 
 function evitarNegativo(e) {
     if (e.key === "-")
